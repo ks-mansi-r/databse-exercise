0633308278b859a13a7a35da40b6de0efa8ab8ed
ks-mansi-r <mansi.rupareliya@kalkani.in>
2025-02-13 10:23:08 +0530

    add transcation

--- src/country/country.service.ts
+++ src/country/country.service.ts
@@ -4,46 +4,96 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Country } from './entity/country.entity';
 import { AddCountryDto } from './dto/add-country.dto';
 import { UpdateCountryDto } from './dto/update-country.dto';
-import { ConflictException, NotFoundException } from '@nestjs/common';
+import { ConflictException, NotFoundException, RequestTimeoutException } from '@nestjs/common';
+import { DataSource } from 'typeorm';
+
 @Injectable()
 export class CountryService {
   constructor(
     @InjectRepository(Country)
     private readonly countryRepository: Repository<Country>,
-  ) {}
+
+    // inject datasource
+    private readonly dataSource: DataSource,
+
+  ) { }
 
   async addCountry(data: AddCountryDto) {
 
-     // if the ISO code already exists, return a 409 Conflict error.
-   try{
-    const exists = await this.countryRepository.findOne({ where: { isoCode: data.isoCode } });
- 
-    
-    const country = this.countryRepository.create(data);
-    return this.countryRepository.save(country);
-   }
-   catch(error){
-    throw new ConflictException(
-      'Data is already exist for this Iso Code',
-      {
-        description:String(error),
-      },
-    );
-  }
+    //Create a query runner
+    const queryRunner = this.dataSource.createQueryRunner();
+
+
+    //connect query runner 
+    await queryRunner.connect();
+
+    //start Transaction
+    await queryRunner.startTransaction();
+    try {
+
+    } catch (error) {
+      throw new RequestTimeoutException(
+        'Not connect to the database',
+      );
+    }
+
+    // if the ISO code already exists, return a 409 Conflict error.
+    try {
+      const exists = await this.countryRepository.findOne({ where: { isoCode: data.isoCode } });
+
+
+      const country = this.countryRepository.create(data);
+      return this.countryRepository.save(country);
+    }
+    catch (error) {
+      throw new ConflictException(
+        'Data is already exist for this Iso Code',
+        {
+          description: String(error),
+        },
+      );
+    }
+    finally{
+      try{
+        //release the connection 
+        await queryRunner.release();
+  
+      }
+      catch(error){
+        throw new RequestTimeoutException(
+  
+          'NOt Release the connection ',
+          {
+            description:String(Error),
+          }
+        );
+  
+      }
+    }
   }
 
   //update country
   async updateCountry(id: number, data: UpdateCountryDto) {
-    const country = await this.countryRepository.findOne({ where: { id } });
-    if (!country) {
-      throw new BadRequestException('Country not found.');
-    }
 
-    Object.assign(country, data);
-    return this.countryRepository.save(country);
+
+    try {
+      const country = await this.countryRepository.findOne({ where: { id } });
+      if (!country) {
+        throw new BadRequestException('Country not found.');
+      }
+
+      Object.assign(country, data);
+      return this.countryRepository.save(country);
+    } catch (error) {
+      throw new NotFoundException(
+        'For this ID country is not found',
+      );
+    }
   }
 
   async deleteCountry(id: number) {
+
+    try{
     const country = await this.countryRepository.findOne({ where: { id }, relations: ['timeseries'] });
 
     if (!country) {
@@ -55,17 +105,22 @@ export class CountryService {
     }
 
     return this.countryRepository.remove(country);
+  }catch(error){
+    throw new NotFoundException(
+      'Data is not available for this date and country'
+    )
+  }
   }
 
   async getCountry(id: number) {
 
     //if the countryId is not found, return a 404 error with an appropriate error message.
-    
-    try{
-    return this.countryRepository.findOne({ where: { id }, relations: ['timeseries'] });
+
+    try {
+      return this.countryRepository.findOne({ where: { id }, relations: ['timeseries'] });
+    }
+    catch (error) {
+      throw new NotFoundException('Country is not found for this id ');
+    }
   }
-  catch(error){
-    throw new NotFoundException('Country is not found for this id ');
-  }
-}
 }
--- src/timeseries/timeseries.service.ts
+++ src/timeseries/timeseries.service.ts
@@ -1,11 +1,11 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import { Injectable, BadRequestException, NotFoundException, ConflictException, RequestTimeoutException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TimeSeries } from './entity/timeseries.entity';
 import { AddTimeSeriesDto } from './dto/add-timeseries.dto';
 import { UpdateTimeSeriesDto } from './dto/update-timeseries.dto';
 import { Country } from 'src/country/entity/country.entity';
-
+import { DataSource } from 'typeorm';
 @Injectable()
 export class TimeSeriesService {
   constructor(
@@ -13,38 +13,102 @@ export class TimeSeriesService {
     private readonly timeSeriesRepository: Repository<TimeSeries>,
     @InjectRepository(Country)
     private readonly countryRepository: Repository<Country>,
-  ) {}
+
+    //Inject datasource 
+    private readonly datasource: DataSource,
+  ) { }
 
   async addTimeSeries(data: AddTimeSeriesDto) {
-    const country = await this.countryRepository.findOne({ where: { id: data.countryId } });
-    if (!country) {
-      throw new BadRequestException('Country not found.');
+
+    // create a query runne5r
+    const queryRunner = this.datasource.createQueryRunner();
+
+    //connect a query runner
+    await queryRunner.connect();
+
+    // Start transcation
+    await queryRunner.startTransaction();
+    try {
+
+    }
+    catch (error) {
+      throw new RequestTimeoutException(
+        'Not connect to database please check it ',
+      );
+
     }
 
-    const timeSeries = this.timeSeriesRepository.create({ ...data, country });
-    return this.timeSeriesRepository.save(timeSeries);
+    try {
+      const country = await this.countryRepository.findOne({ where: { id: data.countryId } });
+      if (!country) {
+        throw new BadRequestException('Country not found.');
+      }
+
+      const timeSeries = this.timeSeriesRepository.create({ ...data, country });
+      return this.timeSeriesRepository.save(timeSeries);
+    } catch (error) {
+
+      //If unsuccessfull then rollback transcation
+      await queryRunner.rollbackTransaction();
+
+      throw new ConflictException(
+        'Not complete the transaction',)
+    }
+    finally {
+
+      try {
+        // Release transcation
+        await queryRunner.release();
+      } catch (error) {
+        throw new RequestTimeoutException('Not release the connection',
+          {
+            description: String(error),
+          });
+      }
+
+    }
   }
 
   async updateTimeSeries(id: number, data: UpdateTimeSeriesDto) {
-    const timeSeries = await this.timeSeriesRepository.findOne({ where: { id } });
-    if (!timeSeries) {
-      throw new BadRequestException('Time series entry not found.');
-    }
 
-    Object.assign(timeSeries, data);
-    return this.timeSeriesRepository.save(timeSeries);
+    try {
+      const timeSeries = await this.timeSeriesRepository.findOne({ where: { id } });
+      if (!timeSeries) {
+        throw new BadRequestException('Time series entry not found.');
+      }
+
+      Object.assign(timeSeries, data);
+      return this.timeSeriesRepository.save(timeSeries);
+    } catch (error) {
+      throw new NotFoundException(
+        'Data is not available for this date and country.',
+      )
+    }
   }
 
   async deleteTimeSeries(id: number) {
-    const timeSeries = await this.timeSeriesRepository.findOne({ where: { id } });
-    if (!timeSeries) {
-      throw new BadRequestException('Time series entry not found.');
-    }
 
-    return this.timeSeriesRepository.remove(timeSeries);
+    try {
+      const timeSeries = await this.timeSeriesRepository.findOne({ where: { id } });
+      if (!timeSeries) {
+        throw new BadRequestException('Time series entry not found.');
+      }
+
+      return this.timeSeriesRepository.remove(timeSeries);
+    } catch (error) {
+
+      throw new NotFoundException(
+        'Data is not available for this date and country'
+      )
+    }
   }
 
   async getTimeSeriesByCountry(countryId: number) {
-    return this.timeSeriesRepository.find({ where: { country: { id: countryId } } });
+    try {
+      return this.timeSeriesRepository.find({ where: { country: { id: countryId } } });
+    }
+    catch (error) {
+      throw new NotFoundException('Data is not available');
+    }
   }
 }
